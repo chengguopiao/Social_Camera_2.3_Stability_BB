0dc550c049dd1d5d1bc7cdec58a865a148ab0017
chengguopiao <chengguo.piao@borqs.com>
2014-11-01 16:48:26 +0800

    Update continuouscapture.py

--- script/continuouscapture.py
+++ script/continuouscapture.py
@@ -60,10 +60,10 @@ class CameraTest(unittest.TestCase):
         #Step 1. Launch single capture activity
         a.cmd('launch','com.intel.camera22/.Camera')
         time.sleep(2)
+        if d(text = 'Yes').wait.exists(timeout = 3000):
+            d(text = 'Yes').click.wait()
         if d(text = 'Skip').wait.exists(timeout = 3000):
-            d(text = 'Skip').click.wait() 
-        if  d(text = 'OK').wait.exists(timeout = 3000):
-            d(text = 'OK').click.wait()
+            d(text = 'Skip').click.wait()
         else:
             assert d(resourceId = 'com.intel.camera22:id/shutter_button'),'Launch camera failed!!'
         sm.switchCaptureMode('Single')    
