c51a5631b2fc29dcce2ffce7ea302679c6ac6c9a
chengguopiao <chengguo.piao@borqs.com>
2014-11-01 16:49:50 +0800

    Update hdrcapture.py

--- script/hdrcapture.py
+++ script/hdrcapture.py
@@ -143,10 +143,10 @@ class CameraTest(unittest.TestCase):
         d.start_activity(component = ACTIVITY_NAME)
         #When it is the first time to launch camera there will be a dialog to ask user 'remember location', so need to check
         try:
-            if d(text = 'Skip').wait.exists(timeout = 3000):
-                d(text = 'Skip').click.wait() 
-            assert d(text = 'OK').wait.exists(timeout = 2000)
-            d(text = 'OK').click.wait()
+            if d(text = 'Yes').wait.exists(timeout = 3000):
+                d(text = 'Yes').click.wait() 
+            assert d(text = 'Skip').wait.exists(timeout = 2000)
+            d(text = 'Skip').click.wait()
         except:
             pass
         assert d(resourceId = 'com.intel.camera22:id/mode_button').wait.exists(timeout = 3000), 'Launch camera failed in 3s'
